09c01f692212b52a1dd7c4eea7d42ef03bafe33e
unknown <BT_3W2_33@BKTECH.LOCAL>
2022-11-10 09:29:10 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String args[]){
+
+
+/*
+
+        int a = 5;
+        int b = 20;
+        while (a < 20 && b > 0) {
+            a++;
+            b--;
+            if (b > 5) {
+                b /= 2;
+            }
+        }
+
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+*/
+        /*
+        int x = 30;
+        int y = 10;
+        int z = 5;
+        System.out.println("x: " + x + ", y: " + y + ", z: " + z);
+
+        while ((x / y >= 0)&&(y != 0) ) {
+            x += 2;
+            y -= 2;
+            if (x % z != 0) {
+                z--;
+            }
+            System.out.println("x: " + x + ", y: " + y + ", z: " + z);
+        }
+*/
+
+
+
+
+
+    }
+}
